09e7fc8e3f9c69100e83ced275e6de968f25ca7e
Mackenzie Malainey <macmalainey@gmail.com>
2018-10-24 07:59:34 -0600

    Created User Model

--- wellness_app/models.py
+++ wellness_app/models.py
@@ -6,5 +6,14 @@ from django.db import models
 # Create your models here.
 class UserInfo(models.Model):
     
-    user_id = models.TextField
+    # Amazon user ID, needed to store the aata specific to a user.
+    user_id = models.CharField(max_length=255)
+
+    wellness_record = models.CharField(max_length=62)
+
+    long_term_record = models.FileField(upload_to='archived_records/')
+
+    
+
+
     
