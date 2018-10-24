a4a82b9758621426ec44b7cd9bb6021a254998f3
Mackenzie Malainey <macmalainey@gmail.com>
2018-10-23 21:46:08 -0600

    Change django app component folder name due to server conflicts

---
+++ wellness_app/apps.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class WellnessbuddyConfig(AppConfig):
+    name = 'wellnessbuddy'
+    verbose_name = 'Amazon Alexa Wellness Buddy'
+
+    
+
---
+++ wellness_app/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class UserInfo(models.Model):
+    
+    user_id = models.TextField
+    
