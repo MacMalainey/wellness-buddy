96c0f89d01b50f809be77955527fed8f57769b01
Mackenzie Malainey <macmalainey@gmail.com>
2018-10-31 09:47:14 -0600

    Changed nullDay task decorator back to shared_Task

--- wellness_app/tasks.py
+++ wellness_app/tasks.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import, unicode_literals
-from celery import app
+from celery import shared_task
 from .models import AlexaUser
 import datetime
 from .processes import appendDataToUserObject
 
-@app.task
+@shared_task
 def nullDay():
     print("Adding null input to all users who have no posted today")
     time = datetime.time(datetime.datetime.now().hour)
