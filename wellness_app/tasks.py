da96ac323a94c7675ead4161b06bc5b37038c13e
Mackenzie Malainey <macmalainey@gmail.com>
2018-10-31 09:32:07 -0600

    Adjusting tasks

--- wellness_app/tasks.py
+++ wellness_app/tasks.py
@@ -1,10 +1,9 @@
 from __future__ import absolute_import, unicode_literals
-from celery import shared_task
 from .models import AlexaUser
 import datetime
 from .processes import appendDataToUserObject
 
-@shared_task
+
 def nullDay():
     print("Adding null input to all users who have no posted today")
     time = datetime.time(datetime.datetime.now().hour)
