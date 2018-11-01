b2848d0b663684b10fcb2515c85cadf68db6a4d9
Lora <lora_ma@hotmail.ca>
2018-11-01 15:33:03 -0600

    update

--- alexa.py
+++ alexa.py
@@ -1,22 +1,4 @@
 <<<<<<< HEAD
-def getWelcomeResponse(callback)
-    speechOutput = 'Hello, I'm your wellness buddy!'
-    repromptText = 'How can I help you today?'
-    shouldEndSession = false
-    callback(sessionAttributes, buildSpeechletResponse(speechOutput, repromptText, shouldEndSession))
-
-def onIntent(intentRequest, session, callback)
-    console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`)
-    intent = intentRequest.intent
-    intentName = intentRequest.intent.name
-
-
-    if (intentName === 'AMAZON.HelpIntent')
-        getWelcomeResponse(callback):
-    else if (intentName === 'AMAZON.StopIntent')
-        handleSessionEndRequest(callback):
-    else if (intentName === 'Start')
-        getStartResponse(callback):
 
 
 =======
