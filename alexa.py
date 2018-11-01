0c31f972e5196c96a0de465c4520ded15cc927b4
Lora <lora_ma@hotmail.ca>
2018-11-01 15:21:11 -0600

    Update

--- alexa.py
+++ alexa.py
@@ -1,2 +1,20 @@
-from django_alexa.api import fields, intent, ResponseBuilder
+def getWelcomeResponse(callback)
+    speechOutput = 'Hello, I'm your wellness buddy!'
+    repromptText = 'How can I help you today?'
+    shouldEndSession = false
+    callback(sessionAttributes, buildSpeechletResponse(speechOutput, repromptText, shouldEndSession))
+
+def onIntent(intentRequest, session, callback)
+    console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`)
+    intent = intentRequest.intent
+    intentName = intentRequest.intent.name
+
+
+    if (intentName === 'AMAZON.HelpIntent')
+        getWelcomeResponse(callback):
+    else if (intentName === 'AMAZON.StopIntent')
+        handleSessionEndRequest(callback):
+    else if (intentName === 'Start')
+        getStartResponse(callback):
+
 
